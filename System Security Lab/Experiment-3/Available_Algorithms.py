9ea51720608ce2a822a432e99e9cb338f7d003bc
Amey Thakur <ameythakur20@gmail.com>
2025-12-24 03:57:25 -0500

    Organize Experiment 3 files and refine code

---
+++ System Security Lab/Experiment-3/Available_Algorithms.py
@@ -0,0 +1,19 @@
+# Experiment 3: Hashing Algorithms (MD5 & SHA-1) - Available Algorithms
+# Course: System Security Lab (CSL604)
+# Name: Amey Thakur (https://github.com/Amey-Thakur)
+# Roll No: 50
+# Batch: B3
+# Repository: https://github.com/Amey-Thakur/CRYPTOGRAPHY-AND-SYSTEM-SECURITY-AND-SYSTEM-SECURITY-LAB
+# Date: 12/03/2021
+
+import hashlib 
+
+"""
+Objective:
+To list all the hashing algorithms guaranteed to be supported by the 'hashlib' module
+across all platforms.
+"""
+
+# prints all available algorithms 
+print ("The available algorithms are : ", end ="") 
+print (hashlib.algorithms_guaranteed) 
